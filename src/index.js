86349677ba2d7529dfdcf66a3701b23cbf99f5d8
Luca Deeken <luca.deeken@fuchs-edv.de>
2024-12-03 12:14:54 +0100

    add modules and add a project class and a todo class. The project class is supposed to store the toDo Cards, which are stored in an object array.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+import { ToDo, Project } from "./objects.js";
+import { sortDOM } from "./populateDom";
+
+
+
+
+const Aufgaben = new Project("Aufgaben");
+Aufgaben.addToDo("fussi", "Muss treten", "1.10.2025", "hoch", "keine AHnung");
+Aufgaben.addToDo("Baskisddf", "Muss tretenlol", "2.11.2025", "hoch", "keine AHnung");
+Aufgaben.addToDo("Baskasdi", "Muss tretenlol", "1.03.2023", "hoch", "keine AHnung");
+Aufgaben.addToDo("Baskisddf", "Muss tretenlol", "2.11.2015", "hoch", "keine AHnung");
+Aufgaben.addToDo("Baskasdi", "Muss tretenlol", "1.03.2029", "hoch", "keine AHnung");
+Aufgaben.addToDo("Baskisddf", "Muss tretenlol", "2.11.2000", "hoch", "keine AHnung");
+Aufgaben.addToDo("Baskasdi", "Muss tretenlol", "1.03.2090", "hoch", "keine AHnung");
+
+// Aufgaben.editName("Tortellini");
+
+// Aufgaben.deleteToDo(0);
+// console.log(Aufgaben);
+
+// Aufgaben.toDos[0].setDone();
+// console.log(Aufgaben);
+
+// Aufgaben.sortToDosByDate();
+// console.log(Aufgaben);
---
+++ src/objects.js
@@ -0,0 +1,72 @@
+import { isAfter, isBefore } from 'date-fns';
+export { ToDo, Project};
+
+
+class ToDo {
+
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = new Date(dueDate);
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = false;
+    }
+
+    setDone() {
+        this.checklist = !this.checklist;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    setDate (dueDate) {
+        this.dueDate = new Date(dueDate);
+    }
+
+    setPriority (priority) {
+        this.priority = priority;
+    }
+
+    setDate (notes) {
+        this.notes = notes;
+    }
+}
+
+class Project {
+    
+    constructor(name) {
+        this.name = name;
+        this.toDos = [];
+    }
+
+    editName(name) {
+        this.name = name;
+    }
+    addToDo(title, description, dueDate, priority, notes) {
+        const newToDo = new ToDo(title, description, dueDate, priority, notes);
+        this.toDos.push(newToDo);
+    }
+
+    deleteToDo(num) {
+        this.toDos.splice(num,1);
+    }
+
+    sortToDosByDate() {
+        this.toDos.sort((a, b) => a.dueDate - b.dueDate);
+    }
+
+}
+
+// Sort by day/week/priority/unchecked
+const sortDOM = {
+
+    sortByDate(Projekt) {
+
+    }
+}
